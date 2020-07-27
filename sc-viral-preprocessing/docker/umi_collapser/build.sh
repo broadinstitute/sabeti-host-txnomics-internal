71c9db268670853175ce5937b6d055c0cff9dec9
Nikolas Barkas <nikolas.barkas@outlook.com>
2020-07-27 09:33:36 -0400

    add missing dockers

---
+++ sc-viral-preprocessing/docker/umi_collapser/build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker build -t us.gcr.io/broad-dsde-methods/sabeti-umi_collapser:0.0.1 .
---
+++ sc-viral-preprocessing/docker/umi_collapser/build.sh~
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker build -t us.gcr.io/broad-dsde-methods/sabeti-bwa:0.7.17 .
